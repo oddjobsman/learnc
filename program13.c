b7252b8a02e142896bddf198930404734e4ab8b6
oddjobsman <oddjobsman@oddjobsman.me>
2014-04-26 18:11:17 +0530

    added programs to demonstrate printing messages and using control characters

---
+++ program13.c
@@ -0,0 +1,7 @@
+/* Program 1.3 Another Simple C Program - Displaying a Quotation */
+#include <stdio.h>
+
+int main(void) {
+	printf("Beware the Ides of March");		/* this line displays the quotations */
+	return 0;
+}
---
+++ program15.c
@@ -0,0 +1,7 @@
+/* Program 1.5 Another Simple C Program - Displaying Great Quotations */
+#include <stdio.h>
+
+int main(void) {
+	printf("\"It is a wise father that knows his child.\"\nShakespeare\n");
+	return 0;
+}
