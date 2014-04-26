393b62cfb11220501c8135bbafcb5c9d73821a6b
oddjobsman <oddjobsman@oddjobsman.me>
2014-04-26 20:15:50 +0530

    added program to demonstrate usage of more variables

---
+++ program23.c
@@ -0,0 +1,15 @@
+/* Program 2.3 - Using more variables */
+#include <stdio.h>
+
+int main(void) {
+	int brothers;
+	int brides;
+	
+	brothers = 7;
+	brides = 7;
+	
+	/* Display the message */
+	printf("%d brothers for %d brides.\n", brothers, brides);
+	
+	return 0;
+}
