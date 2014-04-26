c4e0d048b61dfe7bdee0ccb53d72ad059ffd1a26
oddjobsman <oddjobsman@oddjobsman.me>
2014-04-26 18:26:30 +0530

    added exercise program to print predefined message verbatim

---
+++ exercise13.c
@@ -0,0 +1,8 @@
+/* Exercise 1.4 - Program to print a predefined message verbatim */
+#include <stdio.h>
+
+int main(void) {
+	printf("\"It's freezing in here\", he said coldly.\n");
+	
+	return 0;
+}
