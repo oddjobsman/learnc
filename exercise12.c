06cf5f0dcc5e1f1ab563915f0a143c0e47d23b6f
oddjobsman <oddjobsman@oddjobsman.me>
2014-04-26 18:24:03 +0530

    added exercise program to print name and address on separate line using single printf

---
+++ exercise12.c
@@ -0,0 +1,9 @@
+/* Exercise 1.2 - Program to print name and address on separate lines
+				  using single printf */
+#include <stdio.h>
+
+int main(void) {
+	printf("Ranganath Kini\n123, One Single Street\nSome City, Some State\nFree Country - 123456\n");
+	
+	return 0;
+}
